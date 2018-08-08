374e8108d60ec6312193b62a7cd444457bc13dd8
Shu Yan <yanshu.usc@gmail.com>
2018-08-08 15:07:41 -0400

    move to hadoop dir

---
+++ examples/streaming/wordCount/run-jobs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if ! $(hadoop fs -test -d wordcount) ; then
+    echo "Creating wordcount directory"
+    hadoop fs -mkdir wordcount
+fi
+if ! $(hadoop fs -test -d wordcount/input) ; then
+    echo "Adding input file(s) to hdfs"
+    hadoop fs -put input/ wordcount/input;
+fi
+if $(hadoop fs -test -d wordcount/output) ; then
+    echo "Removing output directory from hdfs"
+    hadoop fs -rm -r wordcount/output
+fi
+
+hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.8.4.jar \
+    -files mapper.py,reducer.py \
+    -input wordcount/input \
+    -output wordcount/output \
+    -mapper mapper.py \
+    -reducer reducer.py
+
